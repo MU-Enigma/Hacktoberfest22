eb4b6e84d248fd3af0779743f9dc3d2ab8dcc6a8
Adnan <m.adnankhan153@gmail.com>
2022-10-14 00:23:36 +0530

    1.0

---
+++ Type_3/Level_1/pascalstriangle.py
@@ -0,0 +1,17 @@
+from math import factorial
+ 
+numRows= input()
+numRows = int(numRows)
+if 1 <= numRows <= 30:
+    for i in range(numRows):
+    for j in range(numRows-i+1):
+
+        print(end=" ")
+ 
+    for j in range(i+1):
+ 
+        print(factorial(i)//(factorial(j)*factorial(i-j)), end=" ")
+ 
+    print()
+    else:
+        print  "Enter a number between 1 and 30!"
