963236544e3d2a0358b25070a84e23ba32bb028f
Shatanshu5355 <shatanshu5355@gmail.com>
2022-10-16 12:46:18 +0530

    Shatanshu_level1_problem4

--- Type_3/Level_1/Shatanshu_level1_problem5.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
- int main(){
-   char s[]= " ";
-   printf("Enter string \n");
-   scanf("%[^\n]s", s);
-   for(int i=strlen(s)-1;i>=0;i--)
-   printf("%c",s[i]);
- }
