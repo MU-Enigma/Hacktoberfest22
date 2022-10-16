6b1cf7bff551f5750505f53553d03dbb93601207
Abhirath <sriabhirath21@gmail.com>
2022-10-16 22:11:27 +0530

    Fixed problem with submission

--- Type_3/Level_1/TheG3ntleman_4.c
+++ Type_3/Level_1/TheG3ntleman_4.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 
 int main() {
+
+    int x;
+TAKE_INPUT:
+    scanf("%d", &x);
     
-    unsigned int x;
-    scanf("%u", &x);
+    if (x < 0) {
+        printf("Invalid Input.\n");
+        goto TAKE_INPUT;
+    }
     
+
     for (unsigned int i = 0; i < 15; i++) {
-        if (i * i < x && x < (i+1) * (i+1)) {
+        if (i * i <= x && x < (i+1) * (i+1)) {
             printf("%u", i);
             break;
         }
     }
-    
+
     return 0;
 }
