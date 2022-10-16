e70d34a29313dfaa0e812404ab96f30194a045a5
naga-tharun <nagatharunmakkena@gmail.com>
2022-10-16 12:15:12 +0530

    level-1_prob-5 done

---
+++ Type_3/Level_1/nagatharun_level1_prob5.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void reverseString(vector<char>& s) {
+    int x = s.size()-1;
+    int n = x+1;
+    for(int i=0; i<n/2; i++){
+        char temp = s[i];
+        s[i] = s[x-i];
+        s[x-i] = temp;
+    }
+}
+
+int main(){
+    string s;
+    cin >> s;
+
+    vector<char> str;
+
+    for(int i=0; i<s.size(); i++){
+        str.push_back(s[i]);
+    }
+
+    reverseString(str);
+    for(int i=0; i<str.size(); i++){
+        cout << str[i];
+    }
+    cout << endl;
+
+    return 0;
+}
