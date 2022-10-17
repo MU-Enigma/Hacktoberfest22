11870241dc4aee6d27975bcfdcff2a6ab53a95cb
Abhirath <sriabhirath21@gmail.com>
2022-10-17 21:27:58 +0530

    Added a solution for Pb. 5, Level. 1, Type 3

---
+++ Type_3/Level_1/TheG3ntleman_5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define MAX_BUFFER_SIZE 105
+
+int main () {
+
+  char str[MAX_BUFFER_SIZE];
+
+  char c;
+  int i = 0;
+  while ((c = getchar()) != '\n') {
+    str[i++] = c;
+  }
+
+  while (i >= 0) {
+    putchar(str[i--]);
+  }
+
+  return 0;
+}
