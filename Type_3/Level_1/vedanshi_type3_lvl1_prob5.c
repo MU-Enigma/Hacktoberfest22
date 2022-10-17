1b3c58a8d10981234d353a47f7e98f1c69a10c4e
Vedanshi <vedanshiverma0512@gmail.com>
2022-10-18 01:24:07 +0530

    ReverseString

--- Type_3/Level_1/vedanshi_type3_lvl1_prob5.c
+++ Type_3/Level_1/vedanshi_type3_lvl1_prob5.c
@@ -1,31 +1,12 @@
-#include<stdio.h>
-void reverse_string(char str[], int start, int end)
-{
-    char temp;
-    while(start<end)
-    {        
-        temp=str[start];
-        str[start]=str[end];
-        str[end]=temp;
-        start++;
-        end--;
-    }
-}
+#include <stdio.h>
+#include <string.h>
 int main()
 {
-    char str[100];
-    scanf("%[^\n]s", str);
-    int start, end;
-    end=0;
-    start=0;
-    while(str[end]!='\0')
+    char a[100];
+    scanf("%[^\n]s", a);
+    int l= strlen(a);
+    for(int i=l-1;i>=0;i--)
     {
-        for(end=start;str[end]!='\0'&&str[end]!=' ';end++);
-        reverse_string(str, start, end-1);
-        start=end+1;
+        printf("%c", a[i]);
     }
-        printf("%s ",str);
-    return 0;
 }
-
-
