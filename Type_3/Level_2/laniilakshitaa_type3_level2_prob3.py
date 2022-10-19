9d916dbb4aa70ffe10da64d2b9c05abfbe44144a
laniilakshitaa <lanii.lakshitaa@gmail.com>
2022-10-19 18:56:34 +0530

    laniilakshitaa_type3_level2_prob3.py

--- Type_3/Level_2/laniilakshitaa_type3_level2_prob3.py
+++ Type_3/Level_2/laniilakshitaa_type3_level2_prob3.py
@@ -1,15 +1,16 @@
 li=input().split()
-li=[int(item) for item in li]
+li=[str(item) for item in li]
 print(li)
 freq={}
-dict=[]
+final=[]
 
 for ele in li:
     if ele in freq:
-        freq[str(ele)]+=1
+        freq[(ele)]+=1
     elif ele not in freq:
-        freq[str(ele)]=1
+        freq[(ele)]=1
 
+print(freq)
 
 k=int(input("k = "))
 
@@ -17,8 +18,10 @@ for i in range(0,k):
     dict_keys=list(freq.keys())
     dict_values=list(freq.values())
     element=dict_keys[dict_values.index(max(dict_values))]
-    dict.append(element)
     freq.pop(str(element))
+    final.append(element)
     
-print(dict)
+    
+print(final)
+
 
