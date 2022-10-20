630b60f4a5b83469c5980b622c76f0283f0bdc43
“siddharth-1303” <siddharthpisipati1303@gmail.com>
2022-10-20 16:32:30 +0530

    added type3 level1 problem2 happy number, fixed not working for single digit

--- Type_3/Level_1/siddharth-1303_TYpe3_Level1_Prob2HappyNumber.py
+++ Type_3/Level_1/siddharth-1303_TYpe3_Level1_Prob2HappyNumber.py
@@ -1,5 +1,11 @@
 num=int(input("enter a number"))
-while(num >= 10):
+if(num<10):
+    if(num==1 or num==7):
+       print("happy number")
+    else:
+        print("not a happy number")
+
+elif(num >= 10):
     s=0
     while(num > 0):    
         rem = num%10    
@@ -8,7 +14,7 @@ while(num >= 10):
         print(s)
     num=s
         
-if  s==1:
-    print("happy number")
-else:
-    print("not a happy number")
+    if  s==1:
+        print("happy number")
+    else:
+        print("not a happy number")
