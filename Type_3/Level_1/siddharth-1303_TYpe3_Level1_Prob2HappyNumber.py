209bb9b191d520ba6e5dabf5733e8c7a0aeaa5c3
“siddharth-1303” <siddharthpisipati1303@gmail.com>
2022-10-20 16:41:58 +0530

    fixed the problem

--- Type_3/Level_1/siddharth-1303_TYpe3_Level1_Prob2HappyNumber.py
+++ Type_3/Level_1/siddharth-1303_TYpe3_Level1_Prob2HappyNumber.py
@@ -1,4 +1,3 @@
-num=int(input("enter a number"))
 if(num<10):
     if(num==1 or num==7):
        print("happy number")
@@ -6,13 +5,14 @@ if(num<10):
         print("not a happy number")
 
 elif(num >= 10):
-    s=0
-    while(num > 0):    
-        rem = num%10    
-        s = s + (rem**2)    
-        num = num//10 
-        print(s)
-    num=s
+    while(num>=10):
+        s=0
+        while(num > 0):    
+            rem = num%10    
+            s = s + (rem**2)    
+            num = num//10 
+            print(s)
+        num=s
         
     if  s==1:
         print("happy number")
