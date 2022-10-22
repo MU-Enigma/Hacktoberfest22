49ae35dbafa73dbae53412f5df386a67a378d931
rushilsgit <rushilsgit>
2022-10-22 18:18:42 +0530

    tyep3-level1-prob3

---
+++ Type_3/Level_1/Rushil_type3_level1_prob3.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+
+void TCSOLVE(){
+    ll n;
+    cin >> n;
+    ll res = 0;
+    for(int i =0;i<n;i++){
+        ll x;
+        cin >> x;
+        res^=x;
+    }    
+    cout << res;
+}
+int main(){
+    ll n;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
+    n = 1;
+    while(n--){
+        TCSOLVE();
+    }
+}
