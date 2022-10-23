d0af28e00714aa800fa7eb08591b28635a88da49
Srikar <t1srikar@gmail.com>
2022-10-23 19:02:58 +0530

    Added Tic-Tac-Toe in C++

---
+++ Type_2/Tic-Tac/Srikar-D-Palmite_tic-tac-toe/tictactoe.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void print(vector<vector<char>> board)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout<<board[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int complete(vector<vector<char>> board)
+{
+    // board must be full or there must be a row or column or diagonal of X or O
+    for (int i = 0; i < 3; i++)
+    {
+        if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][2] != '-')
+            return 1;
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[2][i] != '-')
+            return 1;
+    }
+    if ((board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[2][2] != '-') || 
+        (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[2][0] != '-'))
+    {
+        return 1;
+    }
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            if (board[i][j] == '-')
+                return 0;
+    return 1;
+}
+
+int main()
+{
+    // initialize board
+    vector<vector<char>> board(3);
+    for (int i = 0; i < 3; i++)
+    {
+        vector<char> row(3);
+        row[0] = row[1] = row[2] = '-';
+        board[i] = row;
+    }
+
+    char turn = 'X';
+    while (complete(board)==0)
+    {
+        int x, y;
+        do
+        {
+            print(board);
+            cout<<"It is "<<turn<<"'s turn. Choose location (x, y)"<<endl;
+            cin>>x>>y;
+            if (x>2 || y>2 || board[x][y]!='-')
+            {
+                cout<<"Invalid move"<<endl;
+            }
+            else{
+                break;
+            }
+        } while(true);
+        board[x][y] = turn;
+        turn = (turn=='X') ? 'O':'X';
+    }
+    print(board);
+    cout<<"Game Over"<<endl;
+
+    return 0;
+}
