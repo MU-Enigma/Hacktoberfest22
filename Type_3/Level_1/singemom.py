7cf40bf3f3a92468015e6faa6ddedf72a4440ef3
dongleeisbae <116519747+dongleeisbae@users.noreply.github.com>
2022-10-24 01:46:12 +0530

    Create singemom.py

---
+++ Type_3/Level_1/singemom.py
@@ -0,0 +1,10 @@
+def singlemom(arr):
+    res = 0
+    for i in arr:
+        res ^= i
+    return res
+
+n=int(input("No of elements in the array: "))
+arr_1=list(map(int, input("Elements of array: ").strip().split()))
+
+print(singlemom(arr_1))
