75f9c3ef2d2e9d8e9b0a59488fde63a056ba8372
saianish03 <saianish2003@gmail.com>
2022-10-23 16:55:39 +0530

    Type_3 Level_1 Problem_5 solved

---
+++ Type_3/Level_1/SaiAnish_Level_1_prob_5.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void reverse(string s){
+    int front=0,back=s.length()-1;
+    for (int i = 0; i < s.length(); i++)
+    {
+        while(front<back){
+            swap(s[front],s[back]);
+            front++; 
+            back--;
+        }
+    }
+    cout<<s;
+}
+
+int main(){
+    string s;
+    cin>>s;
+    reverse(s);
+}
