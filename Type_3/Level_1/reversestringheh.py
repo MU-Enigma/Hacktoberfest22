025264fb86ca12f66c0777ffaa179357674f8846
Johaan Vinu Alexander <73550088+coneshawarma@users.noreply.github.com>
2022-10-24 02:28:32 +0530

    Update reversestringheh.py

--- Type_3/Level_1/reversestringheh.py
+++ Type_3/Level_1/reversestringheh.py
@@ -1,5 +1,7 @@
-string  = input("Enter the string")
-
-reverseString = string[::-1]
-
-print(reverseString)
+st=input("Enter a string: ")
+st=list(st)
+for i in range(((len(st))//2)):
+   temp=st[-1-i]
+   st[-1-i]=st[i]
+   st[i]=temp
+print(list(st))
