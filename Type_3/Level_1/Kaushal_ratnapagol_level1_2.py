af0852752f3faa3143b39b394dfe811ef543e463
Kaushalrr006 <kaushalratnapagol@gmail.com>
2022-10-25 10:18:31 +0530

    Kaushal_ratnapagol_level1_2

---
+++ Type_3/Level_1/Kaushal_ratnapagol_level1_2.py
@@ -0,0 +1,24 @@
+#kaushal_ratnapagol = @kaushalrr006
+
+def isHappy(num):  
+    rem = sum = 0;  
+
+    while(num > 0):  
+        rem = num%10;  
+        sum = sum + (rem*rem);  
+        num = num//10;  
+    return sum;  
+      
+
+if __name__ == "__main__":
+    num = int(input("Enter a Number to Determine its Happy status\n-->"))  
+    result = num;  
+    
+    while(result != 1 and result != 4):  
+        result = isHappy(result);  
+
+    if(result == 1):  
+        print("true");  
+    elif(result == 4):  
+        print("false");     
+
