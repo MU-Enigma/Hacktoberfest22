af0f00d60e2cc8f1d3effe85b6352673774ec826
SRV332003 <91906699+SRV332003@users.noreply.github.com>
2022-10-26 12:17:18 +0530

    Update srv332003-reverse-string.py

--- Type_3/Level_3/srv332003-reverse-string.py
+++ Type_3/Level_3/srv332003-reverse-string.py
@@ -1,7 +1,7 @@
-arr=["h","e","l","l","o"]
-
-def reverse():
+#arr=["h","e","l","l","o"]
+arr=input()
+def reverse(arr):
 	for i in range(len(arr)//2):
 		arr[i],arr[len(arr)-1-i]=arr[len(arr)-1-i],arr[i]
-reverse()
+reverse(arr)
 print(arr)
