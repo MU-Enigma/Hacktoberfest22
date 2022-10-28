c363ad0d68475c0c2a5ef4cadc73a91556855ef5
shriyaxd <maniacmelo7@gmail.com>
2022-10-28 19:02:47 +0530

    solved Type3 Level2 Problem5

---
+++ Type_3/Level_2/Shriya_Type3_Lvl2_p5_evaluateExp.cpp
@@ -0,0 +1,89 @@
+//Hacktoberfest22-Type3-l2-P5_Evaluate
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main()
+{
+    int len,val=0, k=0;
+    char pf[100];
+    string s;
+    cin>>s;
+
+    len=s.length();
+
+    for (int i = 0; i<len; i++)
+    {
+        if(s[i]!=' ')
+        {
+            pf[k]=s[i];
+            k++;
+        }
+    }
+    
+    for (int i = 0; i<=len ; i++)
+    {
+        if (pf[i]=='/' || pf[i]=='*')
+        {
+           if (pf[i]=='/')
+            {
+                val=(pf[i-1]-48)/(pf[i+1]-48);
+                pf[i-1]=val+'0';
+                for (int k = i; k<=len;k++)
+                {
+                    pf[k]=pf[k+2];
+                }
+                
+                len=len-2;
+                i--;
+            }
+            else if (pf[i]=='*')
+            {
+                val=(pf[i-1]-48)*(pf[i+1]-48);
+                pf[i-1]=val+'0';
+                for (int k = i; k<=len;k++)
+                {
+                    pf[k]=pf[k+2];
+                }
+
+                len=len-2;
+                i--; 
+            }
+        }
+
+    }
+
+    for (int i=0; i<=len ; i++)
+    {
+        if (pf[i]=='+' || pf[i]=='-')
+        {
+            if (pf[i]=='+')
+            {
+                val=(pf[i-1]-48)+(pf[i+1]-48);
+                pf[i-1]=val+'0';
+                for (int k = i; k<=len;k++)
+                {
+                    pf[k]=pf[k+2];
+                }
+                len=len-2;
+                i--; 
+            }
+
+            else if (pf[i]=='-')
+            {
+                val=(pf[i-1]-48)-(pf[i+1]-48);
+                pf[i-1]=val+'0';
+                for (int k = i; k<=len;k++)
+                {
+                    pf[k]=pf[k+2];
+                }
+                len=len-2;
+                i--;
+            }
+        }
+        
+    }
+ 
+    cout<<pf[0]-'0'<<endl; 
+    return 0;
+}
