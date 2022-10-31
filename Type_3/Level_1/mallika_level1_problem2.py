b3432d933a6a27c792de3b669d0df97f62a5d28d
Mallika <mallikaasthana2002@gmail.com>
2022-10-31 14:02:22 +0530

    Level 1 problem 2 solved

---
+++ Type_3/Level_1/mallika_level1_problem2.py
@@ -0,0 +1,23 @@
+def happynum(num):
+    sum=0
+    for i in str(num):
+        sum=sum+int(i)**2
+    return sum
+
+if __name__ == "__main__":
+    num=int(input())
+    flag=0
+    list=[]
+    while num!=1:
+        list.append(num)
+        num = happynum(num)
+        if num in list:
+            print("false")
+            flag=1
+            break
+    if flag==0:
+        print("true")
+        
+
+
+
